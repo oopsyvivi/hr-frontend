275e7b7c978531ab76bd91d6b79013442ef9db13
oopsyvivi <kyawtmhue.kk@gmail.com>
2026-04-05 15:12:59 +0630

    Add Production Environment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,19 @@
-import axios from "axios";
-const API=axios.create({
-    baseURL:"http://localhost:8080/api",
-});
-export default API;
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// Add token to every request
+API.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default API
